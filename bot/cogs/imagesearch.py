c7f151b87f940a8eedfbae05b3b72b2c02d9c0c2
Colorblind22 <jaydendierker@gmail.com>
2021-11-24 22:48:52 -0600

    used fstrings instead of concatenating and used with open() instead of opening and closing

--- bot/cogs/imagesearch.py
+++ bot/cogs/imagesearch.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-from pybooru import Danbooru as danbooru
+from pybooru import Danbooru 
 import nekos
 from pygelbooru import Gelbooru
 
@@ -8,36 +8,31 @@ data_folder = 'C:/more like shithub/discord-bot-master/data/'
 
 class FileHandler:
     
-    async def danbooru_write(self, tag):  
-        f = open(data_folder+'danboorudata.txt', 'a')
-        if tag is None:
-            tag = 'random'
-        f.write(tag + "\n")
-        f.close()
-        await self.update('danbooru')
-    
-    async def update(self, board):
-        file = open(data_folder+board+'data.txt', 'r')
-        results = dict()
-        lines = file.read().splitlines()
-        for line in lines:
-            for tag in line.split(" "):
-                results[tag] = 0
-        for index in lines:
-            for tag in index.split(" "):
-                results[tag] += 1
-        f = open(data_folder+board+'tagfrequency.txt', 'w')
-        for x, y in sorted(results.items(), reverse=True, key=lambda x: x[1]):
-            f.write(str(y) + ' - ' + x + '\n')
-        f.close()
-        file.close()
+    async def leader_update(self, board):
+        with open(f'{data_folder}{board}data.txt', 'r') as file:
+            results = dict()
+            lines = file.read().splitlines()
+            for line in lines: # instantiate the dictionary with all of the tags, value set to 0
+                for tag in line.split(" "):
+                    results[tag] = 0
+            for index in lines: # loop through searches and add 1 to each value's spot in the dictionary
+                for tag in index.split(" "):
+                    results[tag] += 1
+        with open(f'{data_folder}{board}tagfrequency.txt', 'w') as f:
+            for x, y in sorted(results.items(), reverse=True, key=lambda x: x[1]):
+                f.write(f'{str(y)} - {str(x)}\n')
 
     async def write(self, tag, file):
-        f= open(data_folder+file+'data.txt', 'a')
-        for x in tag:
-            f.write(x + '\n')
-        f.close()
-        await self.update(file)
+        if tag is None:
+            tag = 'random'
+        with open(f'{data_folder}{file}data.txt', 'a') as f:
+            for x in tag:
+                f.write(f'{x}\n')
+        await self.leader_update(file)
+
+    async def error_log(self, post):
+        with open(f'{data_folder}ERRONEOUS_POSTS.txt', 'a') as f:
+            f.write(f'{post}\n\n')
 
 class Private:
     danbooru_api_key = 'HE3uUzSG85HGgYtCQLmd5zRF'
@@ -46,7 +41,7 @@ class Private:
     gelbooru_user_id = '884983'
 
 data = Private()
-client = danbooru(site_name='danbooru', api_key=data.danbooru_api_key)
+danbooru = Danbooru(site_name='danbooru', api_key=data.danbooru_api_key)
 gelbooru = Gelbooru(api_key=data.gelbooru_api_key, user_id=data.gelbooru_user_id)
 fh = FileHandler()
 
@@ -59,20 +54,15 @@ class ImageSearch(commands.Cog):
     async def on_ready(self):
         print('danbooru cog loaded for pictures of ankha for kody')
 
-    @commands.command()
-    async def ankha(self, ctx):
-        await ctx.send(client.post_list(limit = 1, tags = 'ankha_(animal_crossing)', random = True)[0]['file_url'])
-        await fh.danbooru_write('ankha_(animal_crossing)')
-
-    @commands.command()
-    async def danbooru(self, ctx, *tag):
+    @commands.command(name='danbooru')
+    async def danbooru_search(self, ctx, *tag):
         async def tagCon():
             if tag is None:
                 return None
             elif len(tag) == 1:
                 return tag[0]
             elif len(tag) == 2:
-                return tag[0] + " " + tag[1]
+                return f'{tag[0]} {tag[1]}'
             elif len(tag) > 2:
                 await tagOverload()
 
@@ -80,17 +70,18 @@ class ImageSearch(commands.Cog):
             await ctx.send('`Only 2 tags can be searched for at a time, random searching`')
             
         t = await tagCon()
+        post = danbooru.post_list(limit = 1, tags = t, random = True)[0]
         try:
-            await fh.danbooru_write(t)
-            await ctx.send(client.post_list(limit = 1, tags = t, random = True)[0]['file_url'])
+            await ctx.send(post['file_url'])
+            await fh.write(t, 'danbooru')
         except KeyError:
-            print('\t\t------------FILE_URL ERROR BYPASSED------------')
-            danbooru(self,ctx,tag)
+            print('\t\t------------FILE_URL ERROR------------')
+            await fh.error_log(post)
         except IndexError:
-            await ctx.send('No posts found for tag ' + str(tag) + ' ' + nekos.textcat())
+            await ctx.send(f'No posts found for tag {str(tag)} {nekos.textcat()}')
 
-    @commands.command()
-    async def gelbooru(self, ctx, *tag):   
+    @commands.command(name='gelbooru')
+    async def gelbooru_search(self, ctx, *tag):   
         y = list(tag)
         try: 
             await ctx.send(str(await gelbooru.random_post(tags=y)))
@@ -106,9 +97,10 @@ class ImageSearch(commands.Cog):
             else:
                 await ctx.send(nekos.img(tag))
         except:
-            await ctx.send('invalid search ' + nekos.textcat())
+            await ctx.send(f'invalid search {nekos.textcat()}')
         finally:
             await fh.write([tag], 'nekos')
+
 
 def setup(client):
     client.add_cog(ImageSearch(client))
